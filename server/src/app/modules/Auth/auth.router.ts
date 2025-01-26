8a33e92faa82c85935fac3d4a669f57805fc52f4
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-01-26 23:11:55 +0600

    update auth route

--- server/src/app/modules/Auth/auth.router.ts
+++ server/src/app/modules/Auth/auth.router.ts
@@ -1,27 +1,32 @@
-import { Router } from "express";
-import validateRequest from "../../middlewares/validateRequest";
-import { userValidation } from "../user/user.validation";
-import { authController } from "./auth.controller";
-import { authValidation } from "./auth.validation";
+import { Router } from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { userValidation } from '../user/user.validation';
+import { authController } from './auth.controller';
+import { authValidation } from './auth.validation';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from './auth.interface';
 
 const authRouter = Router();
-authRouter.post("/register",
-    validateRequest(userValidation.userValidationSchema),
-    authController.register
-
-)
-authRouter.post("/login",
-    validateRequest(authValidation.loginValidationSchema),
-    authController.login
-
-)
-authRouter.post("/refresh-token",
-    validateRequest(authValidation.refreshTokenValidationSchema),
-    authController.refreshToken
-
-)
-authRouter.post("/logout",
-    authController.logOut
-
-)
+authRouter.post(
+  '/register',
+  validateRequest(userValidation.userValidationSchema),
+  authController.register,
+);
+authRouter.post(
+  '/login',
+  validateRequest(authValidation.loginValidationSchema),
+  authController.login,
+);
+authRouter.post(
+  '/refresh-token',
+  validateRequest(authValidation.refreshTokenValidationSchema),
+  authController.refreshToken,
+);
+authRouter.post(
+  '/update-password',
+  auth(USER_ROLE.customer, USER_ROLE.admin),
+  validateRequest(authValidation.updatePasswordValidationSchema),
+  authController.updatePassword,
+);
+authRouter.post('/logout', authController.logOut);
 export default authRouter;

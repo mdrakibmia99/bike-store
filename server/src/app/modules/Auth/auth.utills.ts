426fc13d95b2c3eaaad6b22a5839b25498f363c0
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-01-26 23:12:21 +0600

    update typescript

--- server/src/app/modules/Auth/auth.utills.ts
+++ server/src/app/modules/Auth/auth.utills.ts
@@ -2,7 +2,7 @@ import jwt, { SignOptions } from 'jsonwebtoken';
 export const createToken = (
   jwtPayload: Record<string, unknown>,
   secret: string,
-  expiresIn: SignOptions['expiresIn'],
+  expiresIn: string | number,
 ) => {
-  return jwt.sign(jwtPayload, secret, { expiresIn });
+  return jwt.sign(jwtPayload, secret, { expiresIn: expiresIn } as SignOptions);
 };

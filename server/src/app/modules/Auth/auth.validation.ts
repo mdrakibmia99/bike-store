9b8ee26538df7377122b0ebe2b58de6f8e2b76ed
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-01-26 23:12:31 +0600

    update auth validation

--- server/src/app/modules/Auth/auth.validation.ts
+++ server/src/app/modules/Auth/auth.validation.ts
@@ -16,9 +16,16 @@ const refreshTokenValidationSchema = z.object({
     refreshToken: z.string({ required_error: 'Refresh token is required' }),
   }),
 });
+const updatePasswordValidationSchema = z.object({
+  body: z.object({
+    oldPassword: z.string({ required_error: 'Old password  is required' }),
+    newPassword: z.string({ required_error: 'New Password is required' }),
+  }),
+});
 
 export const authValidation = {
   loginValidationSchema,
-  refreshTokenValidationSchema
+  refreshTokenValidationSchema,
+  updatePasswordValidationSchema
 };
 export type TUserRole = keyof typeof USER_ROLE;

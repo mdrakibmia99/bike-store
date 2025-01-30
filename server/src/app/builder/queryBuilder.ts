0ea19cb80bd0081560075b2231a022eeab8e95a5
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-01-30 22:30:06 +0600

    update querybuilder

--- server/src/app/builder/queryBuilder.ts
+++ server/src/app/builder/queryBuilder.ts
@@ -1,74 +1,82 @@
-import { FilterQuery, ObjectId, Query } from 'mongoose'
+import { FilterQuery, Query } from 'mongoose';
 
 class QueryBuilder<T> {
-  public modelQuery: Query<T[], T>
-  public query: Record<string, unknown>
+  public modelQuery: Query<T[], T>;
+  public query: Record<string, unknown>;
+
   constructor(modelQuery: Query<T[], T>, query: Record<string, unknown>) {
-    this.modelQuery = modelQuery
-    this.query = query
+    this.modelQuery = modelQuery;
+    this.query = query;
   }
 
   search(searchableFields: string[]) {
-    const search = this?.query?.search || ''
-    this.modelQuery = this.modelQuery.find({
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      $or: searchableFields.map((field: any) => ({
-        [field]: { $regex: search, $options: 'i' },
-      })),
-    } as FilterQuery<T>)
-
-    return this
-  }
-
-  filter() {
-    const queryObj = { ...this.query }
-    const excludingImportant = [
-      'search',
-      'sortOrder',
-      'sortBy'
-    ]
-    
-    // jesob field amdr filtering a drkr nei sesob baad dicchi
-    excludingImportant.forEach((key) => delete queryObj[key])
-  
-    console.log(queryObj,excludingImportant,"queryObj")
-     if(queryObj?.filter){
-
-       this.modelQuery = this.modelQuery.find({author:{_id:(queryObj.filter as ObjectId)}});
-     }
+    const searchTerm = this?.query?.searchTerm;
+    if (searchTerm) {
+      this.modelQuery = this.modelQuery.find({
+        $or: searchableFields.map(
+          (field) =>
+            ({
+              [field]: { $regex: searchTerm, $options: 'i' },
+            }) as FilterQuery<T>,
+        ),
+      });
+    }
 
     return this;
   }
 
+  filter() {
+    const queryObj = { ...this.query }; // copy
 
+    // Filtering
+    const excludeFields = ['searchTerm', 'sort', 'limit', 'page', 'fields'];
 
-  sort() {
-    let sortStr= '-createdAt'
+    excludeFields.forEach((el) => delete queryObj[el]);
 
-    if (this?.query?.sortBy && this?.query?.sortOrder) {
-      const sortBy = this?.query?.sortBy
-      const sortOrder = this?.query?.sortOrder
-      // "-price" othoba "price"
-      sortStr = `${sortOrder === 'desc' ? '-' : ''}${sortBy}`
-    }
-    //  console.log(sortStr,"sortStr")
-    this.modelQuery = this.modelQuery.sort(sortStr)
+    this.modelQuery = this.modelQuery.find(queryObj as FilterQuery<T>);
 
-    return this
+    return this;
   }
 
-  // select() {
-  //   let fields = '-__v'
+  sort() {
+    const sort =
+      (this?.query?.sort as string)?.split(',')?.join(' ') || '-createdAt';
+    this.modelQuery = this.modelQuery.sort(sort as string);
 
-  //   if (this?.query?.fields) {
-  //     fields = (this?.query.fields as string)?.split(',').join(' ')
-  //   }
+    return this;
+  }
 
-  //   this.modelQuery = this.modelQuery.select(fields)
+  paginate() {
+    const page = Number(this?.query?.page) || 1;
+    const limit = Number(this?.query?.limit) || 10;
+    const skip = (page - 1) * limit;
 
-  //   return this
-  // }
+    this.modelQuery = this.modelQuery.skip(skip).limit(limit);
+
+    return this;
+  }
+
+  fields() {
+    const fields =
+      (this?.query?.fields as string)?.split(',')?.join(' ') || '-__v';
+
+    this.modelQuery = this.modelQuery.select(fields);
+    return this;
+  }
+  async countTotal() {
+    const totalQueries = this.modelQuery.getFilter();
+    const total = await this.modelQuery.model.countDocuments(totalQueries);
+    const page = Number(this?.query?.page) || 1;
+    const limit = Number(this?.query?.limit) || 10;
+    const totalPage = Math.ceil(total / limit);
+
+    return {
+      page,
+      limit,
+      total,
+      totalPage,
+    };
+  }
 }
-
 
 export default QueryBuilder;

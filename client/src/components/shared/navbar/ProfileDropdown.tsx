ca110de29977533e228f4ad5147c271838960c01
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-01-30 22:29:51 +0600

    update profile

--- client/src/components/shared/navbar/ProfileDropdown.tsx
+++ client/src/components/shared/navbar/ProfileDropdown.tsx
@@ -1,9 +1,7 @@
 import {
   DropdownMenu,
   DropdownMenuContent,
-
   DropdownMenuItem,
-
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
@@ -11,17 +9,16 @@ import { toast } from "sonner";
 import { useLogOutMutation } from "@/redux/features/auth/authApi";
 import { useAppDispatch } from "@/redux/hooks";
 import { logout } from "@/redux/features/auth/authSlice";
+import { Link } from "react-router-dom";
 
 export function ProfileDropdown() {
   const dispatch = useAppDispatch();
   const [logOut] = useLogOutMutation();
-  const handleLogOut=async()=>{
- 
-      dispatch(logout());
-      toast.success("logout!");
-      await logOut({});
-
-  }
+  const handleLogOut = async () => {
+    dispatch(logout());
+    toast.success("logout!");
+    await logOut({});
+  };
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -34,8 +31,20 @@ export function ProfileDropdown() {
         </Avatar>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-48 mt-8 p-2  ">
-        <DropdownMenuItem className="cursor-pointer border-b hover:text-primary-red">Profile</DropdownMenuItem>
-        <DropdownMenuItem className="cursor-pointer hover:text-primary-red" onClick={handleLogOut}>Log out</DropdownMenuItem>
+        <DropdownMenuItem className="cursor-pointer border-b hover:text-primary-red">
+          Profile
+        </DropdownMenuItem>
+        <Link to="/dashboard">
+          <DropdownMenuItem className="cursor-pointer border-b hover:text-primary-red">
+            Dashboard
+          </DropdownMenuItem>
+        </Link>
+        <DropdownMenuItem
+          className="cursor-pointer hover:text-primary-red"
+          onClick={handleLogOut}
+        >
+          Log out
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );

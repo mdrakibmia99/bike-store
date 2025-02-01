0aced09b51609012de51d1d1d9cd7d7e6cc4c4df
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-02-02 03:01:56 +0600

    update banner

--- client/src/components/Home/Banner.tsx
+++ client/src/components/Home/Banner.tsx
@@ -1,48 +1,87 @@
-// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-
-// import required modules
+import "swiper/swiper-bundle.css";
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
-import { heroSLider } from "@/data/imageLink";
 
-export default function App() {
-  return (<div className="container mx-auto"> 
-    <div className="relative w-full ">
-      <Swiper
-        spaceBetween={30}
-        centeredSlides={true}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Autoplay, Pagination, Navigation]}
-        className="mySwiper h-[60vh]"
-      >
-        <SwiperSlide>
-          <img
-            src={heroSLider.Image1}
-            alt="Royal Enfield"
-            className="w-full h-full object-cover"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src={heroSLider.Image2}
-            alt="Royal Enfield"
-            className="w-full h-full object-cover"
-          />
-        </SwiperSlide>
-      </Swiper>
-    </div>
+import img1 from "@/assets/images/hero-image/slider1.jpg";
+import img2 from "@/assets/images/hero-image/slider2.jpg";
+import img3 from "@/assets/images/hero-image/slider3.jpg";
+
+export default function HeroSlider() {
+  return (
+    <div className="container mx-auto">
+      <div className="relative w-full">
+        <Swiper
+          spaceBetween={30}
+          centeredSlides={true}
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper h-[60vh]"
+        >
+          {/* Slide 1 */}
+          <SwiperSlide>
+            <div className="relative w-full h-full">
+              <img
+                src={img1}
+                alt="Royal Enfield"
+                className="w-full h-full object-cover opacity-70"
+              />
+              <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center text-center text-white px-4">
+                <h1 className="text-4xl md:text-5xl font-bold animate-fadeIn">
+                  Welcome to Royal Knight Bikes
+                </h1>
+                <p className="text-lg md:text-xl mt-2 animate-fadeIn delay-200">
+                  Find the perfect ride for your adventure!
+                </p>
+              </div>
+            </div>
+          </SwiperSlide>
+
+          {/* Slide 2 */}
+          <SwiperSlide>
+            <div className="relative w-full h-full">
+              <img
+                src={img2}
+                alt="Powerful Performance"
+                className="w-full h-full object-cover opacity-70"
+              />
+              <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center text-center text-white px-4">
+                <h1 className="text-4xl md:text-5xl font-bold animate-fadeIn">
+                  Unleash the Power
+                </h1>
+                <p className="text-lg md:text-xl mt-2 animate-fadeIn delay-200">
+                  Experience superior performance and speed.
+                </p>
+              </div>
+            </div>
+          </SwiperSlide>
+
+          {/* Slide 3 */}
+          <SwiperSlide>
+            <div className="relative w-full h-full">
+              <img
+                src={img3}
+                alt="Ultimate Comfort"
+                className="w-full h-full object-cover opacity-70"
+              />
+              <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center text-center text-white px-4">
+                <h1 className="text-4xl md:text-5xl font-bold animate-fadeIn">
+                  Ride with Comfort & Style
+                </h1>
+                <p className="text-lg md:text-xl mt-2 animate-fadeIn delay-200">
+                  Designed for ultimate comfort on long journeys.
+                </p>
+              </div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </div>
   );
 }
